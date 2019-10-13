65ea9d23b300df2cd21fe09a1abb095c7141874a
oreligr <oreligr2@gmail.com>
2019-10-13 17:17:59 +0300

    page home markup

--- page-home.php
+++ page-home.php
@@ -6,11 +6,100 @@
 
 <?php get_header(); ?>
 
+
+	<section class="first-screen" style="background-image: url(./assets/img/first-bg.jpg);">
+		<div class="first-screen_opacity"></div>
+		<div class="container">
+				<h1 class="h1 line">Бесплатный аудит перевода одного языка на вашем сайте</h1>
+				<div class="h1_description">Оставьте нам заявку прямо сейчас, и мы бесплатно проведем аудит вашего сайта</div>
+				<div class="text-center">
+						<a href="#" class="btn btn-secondary">Просчитать проект</a>
+				</div>
+				<div class="block-plus">
+						<div class="block-plus_item">
+								Квалифицированные переводчики по каждой тематике
+						</div>
+						<div class="block-plus_item">
+								Гарантия соблюдения форматирования при любом исходном формате документа
+						</div>
+						<div class="block-plus_item">
+								Использование передового лицензированного программного обеспечения для перевода и подготовки и перевода документов
+						</div>
+				</div>
+			</div>
+	</section>
+
+
+	
+	<section class="section section-about">
+			<div class="container">
+					<h2 class="h2 line">О нашей компании</h2>
+					<div class="h2_description">C помощью этого онлайн-генератора рыботекста можно пачками плодить как отдельные предложения, так и целые абзацы отменнейшего рыбы-текста. А для любителей автоматизации даже реализован API фиштекста.</div>
+					<div class="block-result">
+							<div class="block-result_item">
+									<div class="number line">6</div>
+									<div class="number_title">лет успешной работы</div>
+							</div>
+							<div class="block-result_item">
+									<div class="number line">1000+</div>
+									<div class="number_title">успешно выполненных проектов</div>
+							</div>
+							<div class="block-result_item">
+									<div class="number line">10000+</div>
+									<div class="number_title">слоев переводимых ежедневно</div>
+							</div>
+							<div class="block-result_item">
+									<div class="number line">12+</div>
+									<div class="number_title">рабочих языков</div>
+							</div>
+					</div>
+			</div>
+	</section>
+
 <?php get_template_part('loops/content', 'home'); ?>
 
 <div class="container">
     <?php echo do_shortcode('[bw-reviews] '); ?>
 </div>
+
+
+	<section class="section section-partners">
+			<div class="container">
+					<h2 class="h2 line">Наши клиенты и партнеры</h2>
+					<div class="slider">
+							<div class="slide">
+									<img src="./assets/img/partners-logo.png" alt="">
+							</div>
+							<div class="slide">
+									<img src="./assets/img/partners-logo.png" alt="">
+							</div>
+							<div class="slide">
+									<img src="./assets/img/partners-logo.png" alt="">
+							</div>
+							<div class="slide">
+									<img src="./assets/img/partners-logo.png" alt="">
+							</div>
+							<div class="slide">
+									<img src="./assets/img/partners-logo.png" alt="">
+							</div>
+							<div class="slide">
+									<img src="./assets/img/partners-logo.png" alt="">
+							</div>
+							<div class="slide">
+									<img src="./assets/img/partners-logo.png" alt="">
+							</div>
+					</div>
+			</div>
+	</section>
+	
+
+	<div class="block-line" style="background-image: url(./assets/img/line-bg.jpg);">
+			<div class="container">
+					<div class="description">При первом заказе скидка 5% вне зависимости от объема</div>
+					<a href="#" class="btn btn_none-border">Заказать проект</a>
+			</div>
+	</div>
+
 
 <section class="section">
     <div class="container">
