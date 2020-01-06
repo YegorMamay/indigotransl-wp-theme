8c1be2fe76e0edcd294fed0db02cdd7063b3561b
YegorMamay <web@brainworks.com.ua>
2020-01-06 18:47:01 +0200

    scrollToElement

--- assets/js/brainworks.js
+++ assets/js/brainworks.js
@@ -165,18 +165,21 @@
         var animationSpeed = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 400;
         var links = $("a");
         links.each(function(index, element) {
-            var $element = $(element), href = $element.attr("href");
-            if (href[0] === "#" || href[0] + href[1] === "/#") {
-                $element.on("click", function(e) {
-                    e.preventDefault();
-                    var adaptiveHref = href[0] === "#" ? href : href.slice(1);
-                    var el = $(adaptiveHref);
-                    if (el.length) {
-                        $("html, body").animate({
-                            scrollTop: el.offset().top
-                        }, animationSpeed);
-                    }
-                });
+            var element$ = $(element), href = element$.attr("href");
+            if (href) {
+                if ((href[0] === "#" || href.slice(0, 2) === "/#") && !(href.slice(1, 3) === "__")) {
+                    element$.on("click", function(e) {
+                        e.preventDefault();
+                        var target$ = $(href[0] === "#" ? href : href.slice(1));
+                        if (target$.length) {
+                            $("html, body").animate({
+                                scrollTop: target$.offset().top
+                            }, animationSpeed);
+                        } else if (href[0] === "/") {
+                            location.href = href;
+                        }
+                    });
+                }
             }
         });
     };
